334cc1a9edda235ba2533cc87a0bea612ab31aac
04alexklink <alex_klink123@hotmail.co.uk>
2021-02-17 16:45:42 +0000

    TransactionsList component created and embedded into App component JSX. Once data being used will embed Transaction components into TransactionsList JSX

---
+++ expense-tracker/src/components/TransactionsList.js
@@ -0,0 +1,17 @@
+import Transaction from './Transaction'
+
+const TransactionsList = () => {
+
+    return (
+        <>
+        <h3>History</h3>
+        <ul id="list">
+            <li id="list li">I will be a Transaction component one day</li>
+            <li id="list li">Me too!</li>
+            <li id="list li">Me three!</li>
+        </ul>
+        </>
+    )
+}
+
+export default TransactionsList
