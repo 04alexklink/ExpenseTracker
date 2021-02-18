52f57fcdbbcdeb37a4ea784775327a7edc716dde
04alexklink <alex_klink123@hotmail.co.uk>
2021-02-18 12:58:32 +0000

    Transaction list maps through each transaction, passing it as a prop to the Transaction component. Transaction component then has a ternary operator in the JSX, checking if transaction.amount > 0, and outputting li with transaction info embedded in it, or if transaction.amount < 0, outputting an li with slightly different li (e.g. with className money-minus, and minus sign before displaying transaction.amount. ALL WORKS!

--- expense-tracker/src/components/Transaction.js
+++ expense-tracker/src/components/Transaction.js
@@ -1,6 +1,19 @@
-const Transaction = () => {
+const Transaction = ( {transaction }) => {
+    console.log(transaction.text, transaction.id, transaction.amount)
+    const amountWithoutPrecedingNegSign = (number) => {
+        const NumberChars = number.toString().split("")
+        NumberChars.shift()
+        return NumberChars.join("")
+    }
     return (
         <>
+        {transaction.amount < 0 ?
+            (<li className="minus" key={transaction.id}>
+           {transaction.text} <span>-£{amountWithoutPrecedingNegSign(transaction.amount)}</span><button className="delete-btn">Delete</button>
+            </li>)  :
+            (<li className="plus" key={transaction.id}>
+            {transaction.text} <span>+£{transaction.amount}</span><button className="delete-btn">Delete</button>
+            </li> )} 
         </>
     )
 }
--- expense-tracker/src/components/TransactionsList.js
+++ expense-tracker/src/components/TransactionsList.js
@@ -5,31 +5,12 @@ import { GlobalContext } from '../context/GlobalState'
 const TransactionsList = () => {
   const {transactions } = useContext(GlobalContext);
 
-  const amountWithoutPrecedingNegSign = (number) => {
-     const NumberChars = number.toString().split("")
-     NumberChars.shift()
-     return NumberChars.join("")
- }
-
     return (
         <>
         <h3>History</h3>
         <ul className="list">
-          {transactions.map((transaction) => {
-            if(transaction.amount < 0) {
-              return (
-                <li className="minus" key={transaction.id}>
-              {transaction.text} <span>-£{amountWithoutPrecedingNegSign(transaction.amount)}</span><button className="delete-btn">Delete</button>
-                </li> 
-                )
-            } else { 
-              return (
-             <li className="plus" key={transaction.id}>
-             {transaction.text} <span>+£{transaction.amount}</span><button className="delete-btn">Delete</button>
-             </li> 
-              )
-            }
-          }          
+          {transactions.map((transaction) => 
+            (<Transaction key={transaction.id} transaction={transaction}/>)
           )}
         </ul>
         </>
