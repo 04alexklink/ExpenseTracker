ad70155e962e2ba41fd2945fca1171b5b3447f91
04alexklink <alex_klink123@hotmail.co.uk>
2021-02-18 17:03:39 +0000

    Delete a transaction action set up in reducer, used in deleteTransaction fn in Global state, and this fn is passed into the globalstate/context provider. Then in Transactions component, this function is brought in and assigned as a variable (const {deleteTransaction} = useContext(GlobalContext)..., and then insert this function into an onClick fn on the delete button. It works, state changes based on this action, filtering out the transaction with an id that is the same as the id passed into the deleteTransaction fn when called in the Transaction component

--- expense-tracker/src/components/Transaction.js
+++ expense-tracker/src/components/Transaction.js
@@ -1,19 +1,26 @@
-const Transaction = ( {transaction }) => {
+import { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+
+const Transaction = ( {transaction } ) => {
 
     const amountWithoutPrecedingNegSign = (number) => {
         const NumberChars = number.toString().split("")
         NumberChars.shift()
         return NumberChars.join("")
     }
+
+    const { deleteTransaction } = useContext(GlobalContext)
+    
     
     return (
         <>
         {transaction.amount < 0 ?
             (<li className="minus" key={transaction.id}>
-           {transaction.text} <span>-£{amountWithoutPrecedingNegSign(transaction.amount)}</span><button className="delete-btn">Delete</button>
+           {transaction.text} <span>-£{amountWithoutPrecedingNegSign(transaction.amount)}</span><button className="delete-btn" onClick={() => {deleteTransaction(transaction.id)}}>Delete</button>
             </li>)  :
             (<li className="plus" key={transaction.id}>
-            {transaction.text} <span>+£{transaction.amount}</span><button className="delete-btn">Delete</button>
+            {transaction.text} <span>+£{transaction.amount}</span><button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>Delete</button>
             </li> )} 
         </>
     )
