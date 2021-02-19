8d81f09c51c1f6ae62f7ea894fb2af9007cc8b20
04alexklink <alex_klink123@hotmail.co.uk>
2021-02-19 13:18:35 +0000

    Created Router folder, and imported Router from express in a transactions router file. Exported this and in server.js (app entry file), brought this transactions router file in and mounted the router to the app. Visit '/' works

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const config = require('./config/config');
+const transactions = require('./lib/routes/transactions')
 
 const app = express()
 const PORT = config.PORT
 
-app.get('/', (req, res) => {
-  res.send("Hello I am the entry point to expense tracker backend")
-})
+// app.get('/', (req, res) => {
+//   res.send("Hello I am the entry point to expense tracker backend")
+// })
+app.use(transactions)
 
 app.listen(PORT, console.log(`I am running on ${PORT}`))
 
